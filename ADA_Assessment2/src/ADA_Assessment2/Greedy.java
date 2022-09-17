778100455d3f76f40faf02576e95de370073e4b5
Callum Clow <nhs9115@autuni.ac.nz>
2022-09-17 22:04:04 +1200

    17/9 starting

--- ADA_Assessment2/src/ADA_Assessment2/Greedy.java
+++ ADA_Assessment2/src/ADA_Assessment2/Greedy.java
@@ -8,10 +8,16 @@ public class Greedy extends Subdivision {
         super(width, height);
     }
 
+    // chooses the best division value with the current piece of land
     @Override
     public ArrayList<Land> calculate() {
         Land startLand = new Land(0, 0, this.width, this.height);
         
+        for (int i = 0; i < startLand.width; i++) {
+            
+            
+            int cost = startLand.height * this.divideCost;
+        }
         
         
         return null;
